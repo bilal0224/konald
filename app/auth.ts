8c7a2d1952325e30a343ca49fd4c782599fd217c
moodercoder <2017n1191@gmail.com>
2024-06-12 15:45:11 +0500

    ic

---
+++ app/auth.ts
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { compare } from 'bcrypt-ts';
+import { getUser } from 'app/db';
+import { authConfig } from 'app/auth.config';
+import { headers } from 'next/headers';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize({ email, password }: any) {
+        let user = await getUser(email);
+        if (user.length === 0) return null;
+        let passwordsMatch = await compare(password, user[0].password!);
+        if (passwordsMatch) return user[0] as any;
+      },
+    }),
+  ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    }
+  }
+});
---
+++ app/db.ts
@@ -0,0 +1,44 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+import { eq } from 'drizzle-orm';
+import postgres from 'postgres';
+import { genSaltSync, hashSync } from 'bcrypt-ts';
+
+// Optionally, if not using email/pass login, you can
+// use the Drizzle adapter for Auth.js / NextAuth
+// https://authjs.dev/reference/adapter/drizzle
+let client = postgres(`${process.env.POSTGRES_URL!}?sslmode=require`);
+let db = drizzle(client);
+
+let users = pgTable('User', {
+  id: serial('id').primaryKey(),
+  email: varchar('email', { length: 64 }),
+  password: varchar('password', { length: 64 }),
+});
+
+// Ensure the User table is created on initialization
+(async () => {
+  try {
+    await client`
+      CREATE TABLE IF NOT EXISTS "User" (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(64) UNIQUE NOT NULL,
+        password VARCHAR(64) NOT NULL
+      );
+    `;
+    console.log('Users table is ready.');
+  } catch (error) {
+    console.error('Error creating users table:', error);
+  }
+})();
+
+export async function getUser(email: string) {
+  return await db.select().from(users).where(eq(users.email, email));
+}
+
+export async function createUser(email: string, password: string) {
+  let salt = genSaltSync(10);
+  let hash = hashSync(password, salt);
+
+  return await db.insert(users).values({ email, password: hash });
+}
